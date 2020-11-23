a4e52877065e23b9dd065ebfdf7b0a9732b4d56c
msakharov <msakharov@zoon.ru>
2020-11-23 21:57:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Run Selenoid service in you local
+
+## Run
+Move to project dir, like `cd my-local-selenoid/`  
+Dowload browsers and start selenoid: `. run-selenoid.sh`
---
+++ run-selenoid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pwd=`pwd`
+
+# Download needed browsers
+docker pull selenoid/vnc:chrome_87.0
+
+# Run Selenoid service
+docker run -d --name selenoid -p 4444:4444 -v /var/run/docker.sock:/var/run/docker.sock \
+       -v "$pwd"/config/:/etc/selenoid/:ro aerokube/selenoid:latest-release
